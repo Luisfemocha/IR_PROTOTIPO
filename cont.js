d030cba4ba15cabcab1cc701e45422b11aafbf60
Luisfemocha <luisfemocha@gmail.com>
2020-10-24 01:01:10 -0500

    Version 1.1
    Se agrega el segundo caso de uso 'WRITE CODE'.
    Se encuentra un error al hacer table.empty().
    Se organiza el estilo de los dos casos de usos.
    Se borra código repetido.

--- cont.js
+++ cont.js
@@ -1,12 +1,13 @@
 let mod= $("#modal");
-let tabla= $('#modalTable')
+let table= $('#modalTable');
+let tit= $('#titModal');
 let but= $("#aceptarModal");
 
 function modalIndex(a){
-    tabla.empty();
+    table.empty();
     switch (a) {
         case 1: InsRep();// install repository
-        case 2: // write code
+        case 2: WriCod();// write code
         case 3: // commit activity
         case 4: // read documentation
         case 5: // extract requirement
@@ -24,14 +25,28 @@ function modalIndex(a){
         case 17: // communicate industry_representative
     }
     mod.modal('show');
+    but.show();
 }
-function InsRep(){ // install repository
-    but.hidden='hidden';
-    tabla.append($("<a download='Repository.java'>PULL</a>").attr('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent("import java.util.*;\nclass main{\n  public static void main(String[] args){\n       System.out.println('Hello world :D');\n  }\n}")));
-    tabla.on('click', function (){
-        console.log("import java.util.*;\nclass main{\n  public static void main(String[] args){\n       System.out.println('Hello world :D');\n  }\n}");
+function InsRep(){ // UC01 - INSTALL REPOSITORY
+    table.empty();
+    tit.html('INSTALL REPOSITORY');
+    but.hide();
+    table.append($("<p>Push the 'pull' button in order to install the student's repository.</p>"));
+    table.append($("<a download='Repository.java' class='btn btn-success btn-lg '>PULL</a>").attr('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent("import java.util.*;\nclass main{\n  public static void main(String[] args){\n       System.out.println('Hello world :D');\n  }\n}")));
+    table.on('click', function (){ console.log("import java.util.*;\nclass main{\n  public static void main(String[] args){\n       System.out.println('Hello world :D');\n  }\n}")});
+}
+
+function WriCod(){ // UC02 - WRITE CODE
+    tit.html('WRITE CODE');
+    but.html('SAVE CODE');
+    table.append($("<p>Write your code and when your ready to save it just push the 'SAVE CODE' button and it'll save it on the console.</p>"));
+    table.append($("<textarea id='wc2' class='form-control'></textarea>"));
+    but.on('click', function(){
+        console.log('SAVING CODE...');
+        console.log($('#wc2').val());
+        table.empty();
         mod.modal('hide');
-    });
+    })
 }
-function guardar(tabla, objeto){}
-function modificar(tabla, objeto, mod){}
+/*function guardar(tabla, objeto){}
+function modificar(tabla, objeto, mod){}*/
